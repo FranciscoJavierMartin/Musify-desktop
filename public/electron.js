582698cfbe9456497d7e87e709d09c5ecb81e33e
Francisco <javiermartincuenca@gmail.com>
2020-09-03 21:19:42 +0200

    Initial commit

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var electron_1 = require("electron");
+var electron_is_dev_1 = __importDefault(require("electron-is-dev"));
+var path_1 = require("path");
+var mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 900,
+        height: 680
+    });
+    mainWindow.loadURL(electron_is_dev_1["default"] ? 'http://localhost:3000' : "file://" + path_1.join(__dirname, '../build/index.html'));
+    if (electron_is_dev_1["default"]) {
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.on('closed', function () { return mainWindow = null; });
+}
+electron_1.app.on('ready', createWindow);
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
---
+++ src/electron.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import isDev from 'electron-is-dev';
+import { join } from 'path';
+
+
+let mainWindow: any;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${join(__dirname, '../build/index.html')}`);
+
+  if(isDev){
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin'){
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if(mainWindow === null){
+    createWindow();
+  }});
